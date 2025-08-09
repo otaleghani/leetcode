7d7930e18da8bd9547605e27625d24060d9bf0e3
Oliviero Taleghani <o.taleghani@talesign.com>
2025-08-09 08:57:21 +0200

    added: Lowest Common Ancestor of a Binary Search Tree

---
+++ algorithms/lowest-common-ancestor-of-a-binary-search-tree/solution.ts
@@ -0,0 +1,28 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function lowestCommonAncestor(
+  root: TreeNode | null,
+  p: TreeNode | null,
+  q: TreeNode | null,
+): TreeNode | null {
+  if (!root || !p || !q) {
+    return null;
+  }
+  // Have they split
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+}
