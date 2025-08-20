a7d144e1ce2dc971f8c0f41a59d610f3a9f5ae09
Oliviero Taleghani <o.taleghani@talesign.com>
2025-08-20 08:41:37 +0200

    added: Remove Element

---
+++ algorithms/remove-element/solution.ts
@@ -0,0 +1,23 @@
+function removeElement(nums: number[], val: number): number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+
+// With 2 pointers is much faster
+function removeElementAlt(nums: number[], val: number): number {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+}
