96f3fc015f2cf388343d55f8dcffe5d33f4d11f4
Oliviero Taleghani <o.taleghani@talesign.com>
2025-10-17 10:43:47 +0200

    added: 328. Odd Even Linked List

--- algorithms/odd-even-linked-list/solution.go
+++ algorithms/odd-even-linked-list/solution.go
@@ -32,3 +32,24 @@ func oddEvenList(head *ListNode) *ListNode {
 	head.Next = result.Next
 	return headResult
 }
+
+// A more elegant solution
+func oddEvenListAlternative(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return head
+	}
+
+	odd := head
+	even := head.Next
+	evenHead := even
+
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+
+	odd.Next = evenHead
+	return head
+}
