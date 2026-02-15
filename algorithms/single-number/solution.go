2a207b58ded59e7b21bb8a4a24f889373aefbe54
Oliviero taleghani <o.taleghani@talesign.com>
2026-02-15 07:51:51 +0100

    136. Single Number

--- algorithms/single-number/solution.go
+++ algorithms/single-number/solution.go
@@ -1,11 +1,20 @@
 package main
 
 func singleNumber(nums []int) int {
-    var result = 0
+	var result = 0
 
-    for i := range len(nums) {
-        result = result ^ nums[i]
-    }
+	for i := range len(nums) {
+		result = result ^ nums[i]
+	}
 
-    return result
+	return result
+}
+
+func singleNumberAlt(nums []int) int {
+	res := nums[0]
+	for i := 1; i < len(nums); i++ {
+		res ^= nums[i]
+	}
+
+	return res
 }
