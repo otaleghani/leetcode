503dfd9c8308abf60827f33f54792ca7eb4a2cd1
Oliviero Taleghani <o.taleghani@talesign.com>
2026-07-07 07:30:04 +0200

    added: 278. First Bad Version

---
+++ algorithms/first-bad-version/soluton.go
@@ -0,0 +1,17 @@
+package main
+
+func firstBadVersion(n int) int {
+    left := 1
+    right := n
+    
+    for left < right {
+        mid := left + (right - left) / 2
+        if isBadVersion(mid) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    
+    return left
+}
